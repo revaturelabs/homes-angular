15d32124b2a6dcf45592027d73176af20482482b
Guillermo <blackprogramming@hotmail.com>
2017-11-14 13:53:09 -0500

    Layout Change

--- angular_scripts/directives.js
+++ angular_scripts/directives.js
@@ -1,1 +1,3 @@
 ﻿var app = angular.module('dir', []);
+
+
--- angular_scripts/route.js
+++ angular_scripts/route.js
@@ -1,19 +1,2 @@
 ﻿var app = angular.module('routes', ['ngRoute']);
 
-  app.config(function ($routeProvider, $locationProvider) {
-
-      $routeProvider
-          .when('/', {
-          templateUrl: 'index.html'
-
-      })
-          .when('/manager', {
-          templateUrl: './Web/Manager/Index.html',
-          controller: 'managerController'
-          })
-
-          .otherwise({
-          redirectTo: "/"
-          });
-      $locationProvider.html5Mode(true);
-  });
