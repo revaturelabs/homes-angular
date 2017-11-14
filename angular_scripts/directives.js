b5d9dcf3cd56a940fe44e83e7d6f0ea7ee92ae42
Guillermo <blackprogramming@hotmail.com>
2017-11-14 14:50:58 -0500

    Angular Added and Tenants Profile

--- angular_scripts/directives.js
+++ angular_scripts/directives.js
@@ -1,3 +1,9 @@
 ﻿var app = angular.module('dir', []);
 
-
+//Tenants Directives
+app.directive('sidebar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/tenant-sidebar.html'
+    };
+});
