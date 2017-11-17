8a888f00b6eeae48f4d80553bf59c783265f9ad3
Alexie Ortiz Falu <alexie_15@hotmail.com>
2017-11-16 22:39:31 -0500

    creation of Real Root Config but empty

---
+++ angular_scripts/configs/routeStart-config.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+var App = angular.module('StartApp', ['ui.router', 'dir'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+     //   $urlRouterProvider.otherwise('/Dashboard');
+        $stateProvider.
+            state('Managers', {
+                url: '/Managers',
+                templateUrl: 'Managers/Index.html'
+            }).
+            state('Providers', {
+                url: '/Providers',
+                templateUrl: 'Providers/Index.html'
+              //  controller: 'HousingController'
+            }).
+            state('Tenants', {
+                url: '/Tenants',
+                templateUrl: 'Tenants/Index.html'
+                //  controller: 'HousingController'
+            });
+
+    })
+    .controller('DashboardController', function ($scope) {
+
+    })
+
+    .controller('DashTenantsController', function ($scope) {
+
+    })
+    .controller('HousingController', function ($scope) {
+        $scope.demo = "This is the Housing View"
+    })
+    .controller('UsersController', function ($scope) {
+        $scope.demo = "This is the Users View"
+    })
+    .controller('AppController', function ($scope, $rootScope) {
+        function CallAddButton(name) {
+            $scope.demo = name;
+        };
+
+        $rootScope.$on("CallAddButton", function (event, name) {
+            CallAddButton(name);
+        });
+    });
+
