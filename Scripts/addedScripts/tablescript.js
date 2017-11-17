8aedeb5c78598a8575c525ba7b9f51bb2426020a
Guillermo <blackprogramming@hotmail.com>
2017-11-16 20:38:01 -0500

    Cleaning code

--- Scripts/addedScripts/tablescript.js
+++ Scripts/addedScripts/tablescript.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
     
      $("[data-toggle=tooltip]").tooltip();
     
-} );
+});
 
-  
+$(document).ready(function () {
+    $('#datatable2').dataTable();
+
+    $("[data-toggle=tooltip]").tooltip();
+
+});
+
+function datatable() {
+    $('#datatable').dataTable();
+
+    $("[data-toggle=tooltip]").tooltip();
+}
 
