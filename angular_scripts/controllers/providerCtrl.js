838f8446bbe8dbe8d7b6d4858c6e234105a78e16
Nelson Miranda <nmirandalo93@outlook.com>
2017-11-20 16:35:56 -0500

    Added Addresses services to addressesSrvc and controllers on providerCtrl.

--- angular_scripts/controllers/providerCtrl.js
+++ angular_scripts/controllers/providerCtrl.js
@@ -1,10 +1,98 @@
-﻿'use strict';
-angular.module('StartApp.providerApp')
-    .controller("managerCtrl", function ($http, $scope) {
+﻿//COMPLETED - Need to review.
 
 
+//'use strict';
+//angular.module('StartApp.providerApp')
+//    .controller("managerCtrl", function ($http, $scope) {
 
 
+//    });
+
+'use strict';
+angular.module('StartApp.managerApp')
+    .controller("provierCtrl", function ($http, $scope) {
 
 
+    })
+
+    .controller('DashManagersController', function ($scope, providerFactory) {
+
+        $scope.status;
+        $scope.addrress;
+        $scope.postAddress;
+        $scope.putAddress;
+        $scope.deleteAddress;
+
+        getManagers();
+
+        function getAddresses() {
+            providerFactory.getAddresses()
+                .then(function (response) {
+                    $scope.managers = response.data;
+                }, function (error) {
+                    $scope.status = 'Unable to load Addresses: ' + error.message;
+                });
+
+        };
+
+        $scope.getAddressById = function getManagersById(id) {
+            providerFactory.getManagersById(id)
+                .then(function (response) {
+                    $scope.addressById = response.data;
+                }, function (error) {
+                    $scope.status = 'Unable to load Address by Id: ' + error.message;
+                });
+
+        };
+
+        $scope.postAddress = function postAddress(address) {
+            providerFactory.postAddress(address)
+                .then(function (response) {
+                    $scope.address = response.data;
+                }, function (error) {
+                    $scope.status = 'Unable to Post Address: ' + error.message;
+                });
+        };
+
+        $scope.putManager = function putManager(address) {
+            providerFactory.putManager(address)
+                .then(function (response) {
+                    $scope.address = response.data;
+                }, function (error) {
+                    $scope.status = 'Unable to Put Address: ' + error.message;
+                });
+        };
+
+        $scope.deleteManager = function deleteManager(id) {
+            providerFactory.deleteManager(id)
+                .then(function (response) {
+                    $scope.address = response.data;
+                }, function (error) {
+                    $scope.status = 'Unable to Delete Address: ' + error.message;
+                });
+        };
+
+    })
+
+    .controller('DashboardController', function ($scope) {
+
+    })
+
+    .controller('DashTenantsController', function ($scope) {
+
+    })
+
+    .controller('DashRecruitersController', function ($scope) {
+
+        $scope.demo = "This is the Dashboard Recruiters View"
+    })
+    .controller('DashProvidersController', function ($scope) {
+
+        $scope.demo = "This is the Dashboard Providers View"
+    })
+    .controller('SuppliesController', function ($scope) {
+        $scope.demo = "This is the Supplies View"
+    })
+    .controller('UsersController', function ($scope) {
+        $scope.demo = "This is the Users View"
     });
--- angular_scripts/services/addressesSrvc.js
+++ angular_scripts/services/addressesSrvc.js
@@ -1,30 +1,82 @@
-﻿'use strict';
-angular.module('app')
-    .factory('addressesSrvc', ['$http', '$scope', function ($http, $scope) {
-        return {
-            getAddresses: function () {
-                return $http.get(server + '/api/Address/');
-            },
-            getAddress: function (id) {
-                return $http({
-                    method: 'GET',
-                    url: server + '/someUrl'
-                    //header: {authorization: "Bearer " + token  
-                    //}
+﻿//COMPLETED - Need to review.
 
-                });
-            },
-            postAddress: function (item) {
-                return $http.post('/api/Address/', item);
-            },
-            putAddress: function (item) {
-                return $http.put('/api/Address/', item);
-            },
-            deleteAddress: function (id) {
-                return $http({
-                    method: 'DELETE',
-                    url: '/api/Address/' + id
-                });
-            }
+
+//'use strict';
+//angular.module('app')
+//    .factory('addressesSrvc', ['$http', '$scope', function ($http, $scope) {
+//        return {
+//            getAddresses: function () {
+//                return $http.get(server + '/api/Address/');
+//            },
+//            getAddress: function (id) {
+//                return $http({
+//                    method: 'GET',
+//                    url: server + '/someUrl'
+//                    //header: {authorization: "Bearer " + token  
+//                    //}
+
+//                });
+//            },
+//            postAddress: function (item) {
+//                return $http.post('/api/Address/', item);
+//            },
+//            putAddress: function (item) {
+//                return $http.put('/api/Address/', item);
+//            },
+//            deleteAddress: function (id) {
+//                return $http({
+//                    method: 'DELETE',
+//                    url: '/api/Address/' + id
+//                });
+//            }
+//        };
+//    }]);
+
+angular.module('StartApp.managerApp')
+    .factory('providerFactory', ['$http', function ($http) {
+
+        var urlBase = 'https://homes-webapi.azurewebsites.net/api/Addresses';
+        var providerFactory = {};
+
+        providerFactory.getAddresses = function () {
+            return $http.get(urlBase);
         };
+
+
+        providerFactory.getAddressById = function (id) {
+            return $http.get(urlBase + '/' + id);
+        };
+
+
+        providerFactory.postAddress = function (address) {
+            $http({
+                method: 'POST',
+                dataType: 'json',
+                url: urlBase,
+                data: { address },
+                headers: { "Content-Type": "application/json" }
+            }).then(function (response) {
+                return response;
+            });
+        };
+
+
+        providerFactory.putAddress = function (address) {
+            $http({
+                method: 'PUT',
+                dataType: 'json',
+                url: urlBase + '/' + address.addressId,
+                data: { address },
+                headers: { "Content-Type": "application/json" }
+            }).then(function (response) {
+                return response;
+            });
+        };
+
+        providerFactory.deleteAddress = function (id) {
+            return $http.delete(urlBase + '/' + id);
+        };
+
+        return providerFactory;
+
     }]);
