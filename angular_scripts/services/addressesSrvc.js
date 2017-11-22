b49af065a4bb533d377835b6ee3708ef41e89129
Guillermo <blackprogramming@hotmail.com>
2017-11-22 13:16:05 -0500

    Fixing bugs in services

--- angular_scripts/services/addressesSrvc.js
+++ angular_scripts/services/addressesSrvc.js
@@ -1,38 +1,4 @@
-﻿//COMPLETED - Need to review.
-
-
-//'use strict';
-//angular.module('app')
-//    .factory('addressesSrvc', ['$http', '$scope', function ($http, $scope) {
-//        return {
-//            getAddresses: function () {
-//                return $http.get(server + '/api/Address/');
-//            },
-//            getAddress: function (id) {
-//                return $http({
-//                    method: 'GET',
-//                    url: server + '/someUrl'
-//                    //header: {authorization: "Bearer " + token  
-//                    //}
-
-//                });
-//            },
-//            postAddress: function (item) {
-//                return $http.post('/api/Address/', item);
-//            },
-//            putAddress: function (item) {
-//                return $http.put('/api/Address/', item);
-//            },
-//            deleteAddress: function (id) {
-//                return $http({
-//                    method: 'DELETE',
-//                    url: '/api/Address/' + id
-//                });
-//            }
-//        };
-//    }]);
-
-angular.module('StartApp.managerApp')
+﻿angular.module('StartApp.managerApp')
     .factory('providerFactory', ['$http', function ($http) {
 
         var urlBase = 'https://homes-webapi.azurewebsites.net/api/Addresses';
@@ -81,7 +47,7 @@ angular.module('StartApp.managerApp')
 
 
         providerFactory.putAddress = function (address) {
-            $http({
+            return $http({
                 method: 'PUT',
                 dataType: 'json',
                 url: urlBase + '/' + address.addressId,
@@ -90,9 +56,7 @@ angular.module('StartApp.managerApp')
                     "Content-Type": "application/json",
                     "Authorization": 'Bearer ' + sessionStorage['adal.access.token.key' + cid]
                 }
-            }).then(function (response) {
-                return response;
-            });
+            })
         };
 
         providerFactory.deleteAddress = function (id) {
