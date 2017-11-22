df822583ecf3a507ee19a4426472c23d748c7f06
gabowatt <gabowatt@gmail.com>
2017-11-21 21:09:06 -0500

    Added Manage Contact Post and Modified Partial.

--- Scripts/addedScripts/addGroup.js
+++ Scripts/addedScripts/addGroup.js
@@ -6,8 +6,8 @@ function contact_fields() {
     var divtest = document.createElement("div");
 	divtest.setAttribute("class", "form-group removeclass"+room);
 	var rdiv = 'removeclass'+room;
-    divtest.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="Name" name="Name[]" value="" placeholder="Name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="Last Name" name="Last Name[]" value="" placeholder="Last Name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="Email" name="Phone Number[]" value="" placeholder="Phone Number"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"><input type="text" class="form-control" id="Email" name="Email[]" value="" placeholder="Email"><div class="input-group-btn"><button class="btn btn-dark" type="button" onclick="remove_contact('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div>';
-    
+    divtest.innerHTML = ; 
+
     objTo.appendChild(divtest)
 }
    function remove_contact(rid) {
