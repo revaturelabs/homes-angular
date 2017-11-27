1ceed09fe202a9faa07e03234207b1b9baddde2b
Brian Matta <brianmatta@hotmail.com>
2017-11-27 00:08:14 -0500

    Added: - angularvalidation.js - added validation for batch name and start date for Create and Update modals - added labels for create modal

---
+++ Scripts/addedScripts/angularvalidation.js
@@ -0,0 +1,13 @@
+﻿var validationApp = angular.module('validationApp', []);
+
+validationApp.controller('mainController', function ($scope) {
+		
+    $scope.submitForm = function () {
+
+        if ($scope.userForm.$valid) {
+            alert('our form is amazing');
+        }
+
+    };
+
+});
