48d82100674f17ec66c77fa83ab29e0ba89d86da
zero542 <zero542@live.com>
2017-11-27 02:44:37 -0500

    Getting Contact Id while sign in , fix some buttons and Maintenances request post almos done

--- angular_scripts/controllers/tenantCtrl.js
+++ angular_scripts/controllers/tenantCtrl.js
@@ -1,8 +1,8 @@
 ﻿'use strict';
 angular.module('StartApp.tenantApp')
-    .controller("TenantsDashboardController", ['$http', '$scope', '$rootScope', 'tenantsFactory', 'supplyRequestsFactory', 'maintenanceRequestsFactory', function ($http, $scope, $rootScope, tenantsFactory, supplyRequestsFactory, maintenanceRequestsFactory) {
+    .controller("TenantsDashboardController", ['$http', '$scope', '$rootScope', 'tenantsFactory', 'supplyRequestsFactory', 'maintenanceRequestsFactory', 'contactFactory', function ($http, $scope, $rootScope, tenantsFactory, supplyRequestsFactory, maintenanceRequestsFactory,contactFactory) {
         //getTenants();
-        var x = 3;
+        //var x = 2;
         //var y = 2;
         //function getTenants() {
         //    tenantsFactory.getTenants()
@@ -15,16 +15,34 @@ angular.module('StartApp.tenantApp')
 
         //};
         $scope.tenant;
-        //getTenantInfo(x);
         
+        //getTenantInfo(x);
+        var x = sessionStorage.oid;
+        console.log(x);
+        
+        console.log(x);
+        $scope.getContactbyoid = function () {
+            contactFactory.getContactbyoid (x)
+                .then(function (response) {
+                    $scope.contact = response.data;
+                    //$scope.y = $scope.tenant.contactId;
+                    //console.log($scope.y);
+                    console.log($scope.contact);
+                    getTenantInfo($scope.contact);
+                }, function (error) {
+                    $scope.status = 'Unable to load Tenants: ' + error.message;
+                });
 
-        $scope.getTenantInfo = function() {
-            tenantsFactory.getTenantInfo(x)
+        };
+
+         function getTenantInfo (y) {
+            tenantsFactory.getTenantInfo(y)
                 .then(function (response) {
                     $scope.tenant = response.data;
                     //$scope.y = $scope.tenant.contactId;
                     //console.log($scope.y);
-                    console.log($scope.tenant.contactId);
+                    console.log($scope.tenant.tenantId)
+                    sessionStorage["tenantid"] = $scope.tenant.tenantId;
                     getSupplyRequestWithSuppliesByTenant($scope.tenant.contactId);
                     getMaintenanceRequestByTenant($scope.tenant.contactId);
                 }, function (error) {
@@ -60,9 +78,29 @@ angular.module('StartApp.tenantApp')
 
 
     }])
-    .controller("TenantsMaintenanceController", function ($http, $scope) {
+    .controller("TenantsMaintenanceController", ['$http', '$scope', '$rootScope', 'maintenanceRequestsFactory', 'contactFactory', function ($http, $scope, $rootScope, maintenanceRequestsFactory, contactFactory) {
 
-    })
+        console.log(sessionStorage.tenantid);
+        $scope.message;
+        $scope.postMaintenanceRequest = function () {
+            var request = JSON.stringify({ tenantId: sessionStorage.tenantid, active: true, message: $scope.message })
+            console.log(request);
+            maintenanceRequestsFactory.postMaintenanceRequest(request)
+                .then(function (response) {
+                    $scope.maintenance = response.data;
+                    console.log($scope.maintenance);
+                }, function (error) {
+                    $scope.status = 'Unable to load Supplies requests: ' + error.message;
+                });
+
+        };
+        
+           
+              
+        
+        
+
+    }])
     .controller("TenantsSuppliesController", function ($http, $scope) {
 
     })
